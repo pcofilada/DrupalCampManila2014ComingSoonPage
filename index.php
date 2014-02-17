de05854c4f15d05e6eae8f19eacbd02cb55f566c
pcofilada <pcofilada@gmail.com>
2014-02-17 15:36:10 +0800

    Added styles

--- index.php
+++ index.php
@@ -10,7 +10,10 @@
         <meta name="description" content="Drupal Camp Manila 2014: Coming Soon Page">
         <meta name="viewport" content="width=device-width">
 		<title>Drupal Camp Manila 2014</title>
+		<link rel="stylesheet" href="css/main.css">
+		<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
 		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+		<link href='http://fonts.googleapis.com/css?family=Lato|Droid+Serif' rel='stylesheet' type='text/css'>
 	</head>
 <body>
     <!--[if lt IE 7]>
@@ -21,31 +24,36 @@
 		<div class="col-lg-6">
 			<h1>Drupal Camp</h1>
 			<h2>Manila 2014</h2>
-			<div id="venue">
-				<p>Sadoce, 12F Smart Tower I</p>
-				<p>Makati City, Philippines</p>
-			</div>
-			<div id="contacts">
-				<div class="contact">
-					<p>Gem Devanadera</p>
-					<p>Drupal Pilipinas Community Head</p>
-					<p>gemdev@drupalpilipinas</p>
+			<div class="details">
+				<div id="venue">
+					<p>Sadoce, 12F Smart Tower I</p>
+					<p>Makati City, Philippines</p>
 				</div>
-				<div class="contact">
-					<p>Poniel Ador</p>
-					<p>Drupal Pilipinas Marketing Head</p>
-					<p>marketing@drupalpilipinas.org</p>
+				<div id="contacts">
+					<div class="contact">
+						<p>Gem Devanadera</p>
+						<p>Drupal Pilipinas Community Head</p>
+						<p>gemdev@drupalpilipinas</p>
+					</div>
+					<div class="contact">
+						<p>Poniel Ador</p>
+						<p>Drupal Pilipinas Marketing Head</p>
+						<p>marketing@drupalpilipinas.org</p>
+					</div>
 				</div>
 			</div>
 		</div>
 		<div class="col-lg-6">
 			<div id="subscribe">
+				<h3>RECIEVE UPDATES ABOUT THE EVENT!</h3>
 				<form action="http://tripbuddy.us3.list-manage1.com/subscribe/post?u=921a2981a49d1bb6aef36abf5&amp;id=9beb363f77" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 					<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Email Address" required>
 				    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 				    <div style="position: absolute; left: -5000px;"><input type="text" name="b_921a2981a49d1bb6aef36abf5_9beb363f77" value=""></div>
 					<div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 				</form>
+				<a href="https://www.facebook.com/DrupalPilipinas" target="_blank"><i class="fa fa-facebook-square"></i></a>
+				<a href="https://twitter.com/DrupalPilipinas" target="_blank"><i class="fa fa-twitter-square"></i></a>
 			</div>
 			<div id="countdown"></div>
 		</div>
